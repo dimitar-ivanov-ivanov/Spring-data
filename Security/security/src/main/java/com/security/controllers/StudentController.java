0336fc83909c11d84b0980a00fdfe39110eed5a9
dimitar-ivanov-ivanov <dimitar.i.ivanov@abv.bg>
2021-03-26 10:38:47 +0200

    Adding folder for spring security with an initial project.

---
+++ Security/security/src/main/java/com/security/controllers/StudentController.java
@@ -0,0 +1,30 @@
+package com.security.controllers;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.security.models.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "Petar"),
+            new Student(2, "Ivan"),
+            new Student(3, "Anna")
+    );
+
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable("studentId") Long studentId) {
+        return STUDENTS
+                .stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Student id does not exist"));
+    }
+}
---
+++ Security/security/src/main/java/com/security/models/Student.java
@@ -0,0 +1,19 @@
+package com.security.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    private long studentId;
+
+    private String studentName;
+
+
+}
