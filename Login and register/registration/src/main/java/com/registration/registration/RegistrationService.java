dae05bc438be4c91dc4afd21920611c7a8bd1dec
dimitar-ivanov-ivanov <dimitar.i.ivanov@abv.bg>
2021-03-31 18:18:30 +0300

    Adding login and register folder with inital model,service,repository and security.

---
+++ Login and register/registration/src/main/java/com/registration/registration/RegistrationService.java
@@ -0,0 +1,11 @@
+package com.registration.registration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistrationService {
+
+    public String register(RegistrationRequest request) {
+        return "works";
+    }
+}
